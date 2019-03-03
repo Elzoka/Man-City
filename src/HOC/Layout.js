58de4c55e3f9d145544cab6f47c1fbd81525e5bd
Elzoka <mahmoud_elzoka@yahoo.com>
2019-03-03 13:26:43 +0200

    add Footer component

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Header from '../Components/Header_Footer/Header';
+import Footer from '../Components/Header_Footer/Footer';
 
 const Layout = (props) => {
     return (
         <div>
             <Header />
             {props.children}
-            footer
+            <Footer />
         </div>
     );
 };
