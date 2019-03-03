f76175a8141ec788fcb6dff753fcd3b6696b75b0
Elzoka <mahmoud_elzoka@yahoo.com>
2019-03-03 16:16:53 +0200

    add Featured component

---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Featured from './Featured';
+
+const Home = () => {
+    return (
+        <div className="bck_blue">
+            <Featured />
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
+import Home from './Components/Home';
 import Layout from './HOC/Layout';
 
 const Routes = props => {
     return (
-      <Layout>
-        Second line
-      </Layout>
+    	<Layout>
+			<Switch>
+				<Route exact component={Home} path="/" />
+			</Switch>
+    	</Layout>
     );
 }
 
