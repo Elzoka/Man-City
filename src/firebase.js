dd1dbb227d8514ce29fb828feb085df6643f8c98
Elzoka <mahmoud_elzoka@yahoo.com>
2019-03-04 18:55:25 +0200

    move config to seperate file

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase';
 import 'firebase/app';
 import 'firebase/database';
 
-const config = {
-    apiKey: "AIzaSyBxBuThB3QKH88oseSHXEgSTCyklN2IbkI",
-    authDomain: "m-city-a1443.firebaseapp.com",
-    databaseURL: "https://m-city-a1443.firebaseio.com",
-    projectId: "m-city-a1443",
-    storageBucket: "m-city-a1443.appspot.com",
-    messagingSenderId: "991904514184"
-};
+import config from './config';
 
 firebase.initializeApp(config);
 
