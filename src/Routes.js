1ae07cd981e93c24b2699e661faefab53c728698
Elzoka <mahmoud_elzoka@yahoo.com>
2019-03-07 12:29:30 +0200

    add basic setup for the admin dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,15 @@ import Home from './Components/Home';
 import Layout from './HOC/Layout';
 import SignIn from './Components/SignIn';
 
+import Dashboard from './Components/admin/Dashboard';
+
 const Routes = props => {
     return (
     	<Layout>
 			<Switch>
 				<Route exact component={Home} path="/" />
 				<Route exact component={SignIn} path="/sign_in" />
+				<Route exact component={Dashboard} path="/dashboard" />
 			</Switch>
     	</Layout>
     );
